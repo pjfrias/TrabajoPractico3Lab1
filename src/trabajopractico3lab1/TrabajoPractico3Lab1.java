b126a30243fe40a746a67a8455bb69979c016eb4
pjfrias <pjfrias@gmail.com>
2024-03-27 18:00:29 -0300

    Armado de clase visual Login

--- src/trabajopractico3lab1/TrabajoPractico3Lab1.java
+++ src/trabajopractico3lab1/TrabajoPractico3Lab1.java
@@ -10,6 +10,11 @@ public class TrabajoPractico3Lab1 {
 
     public static void main(String[] args) {
      
+        Login ventana = new Login();
+        
+        ventana.setVisible(true);
+        ventana.setLocationRelativeTo(null);
+        
     }
 
 }
