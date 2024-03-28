bf7640d8c40c7e37791884608560ec49737bfc05
Alex Nahuel Corales <alexcorales21@gmail.com>
2024-03-28 19:10:53 -0300

    agregue metodos

--- src/trabajopractico3lab1/TrabajoPractico3Lab1.java
+++ src/trabajopractico3lab1/TrabajoPractico3Lab1.java
@@ -1,17 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package trabajopractico3lab1;
 
 public class TrabajoPractico3Lab1 {
 
     public static void main(String[] args) {
-     
         Login ventana = new Login();
-        
         ventana.setVisible(true);
         ventana.setLocationRelativeTo(null);
         
