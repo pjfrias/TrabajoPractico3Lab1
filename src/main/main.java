bd538428bbe4726212575c394567ab97f37c4a06
Alex Nahuel Corales <alexcorales21@gmail.com>
2024-04-03 17:03:44 -0300

    Update main.java

--- src/main/main.java
+++ src/main/main.java
@@ -6,13 +6,19 @@ import vista.Login;
 public class Main {
 
     public static void main(String[] args) {
-        try{
+        
+        Login ventana = new Login();
+        ventana.setVisible(true);
+        ventana.setLocationRelativeTo(null);
+        
+        /*try{
             Login ventana = new Login();
             ventana.setVisible(true);
             ventana.setLocationRelativeTo(null);
         }catch(Exception e){
             JOptionPane.showMessageDialog(null, "Ocurrio un problema al iniciar");
-        }
+            System.out.println("Error: " + e.getMessage());
+        }*/
     }
 
 }
