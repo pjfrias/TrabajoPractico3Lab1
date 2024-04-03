ed508c16f4045dfa74d723417b3f800f53f76f5f
Alex Nahuel Corales <alexcorales21@gmail.com>
2024-04-03 17:00:46 -0300

    try catch main

--- src/main/main.java
+++ src/main/main.java
@@ -1,14 +1,18 @@
 package main;
 
+import javax.swing.JOptionPane;
 import vista.Login;
 
 public class Main {
 
     public static void main(String[] args) {
-        Login ventana = new Login();
-        ventana.setVisible(true);
-        ventana.setLocationRelativeTo(null);
-        
+        try{
+            Login ventana = new Login();
+            ventana.setVisible(true);
+            ventana.setLocationRelativeTo(null);
+        }catch(Exception e){
+            JOptionPane.showMessageDialog(null, "Ocurrio un problema al iniciar");
+        }
     }
 
 }
